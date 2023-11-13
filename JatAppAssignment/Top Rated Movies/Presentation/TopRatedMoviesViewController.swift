abb885ee2919943e11b1fd22f673097cf8b57ffc
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2023-11-13 11:22:43 +0200

    Add basic table view implementation to the `TopRatedMoviesViewController`

--- JatAppAssignment/Top Rated Movies/Presentation/TopRatedMoviesViewController.swift
+++ JatAppAssignment/Top Rated Movies/Presentation/TopRatedMoviesViewController.swift
@@ -6,7 +6,51 @@ import UIKit
 
 class TopRatedMoviesViewController: UIViewController {
 
+    // MARK: - Private properties
+    private let tableView = UITableView()
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+        configureTableView()
+    }
+    
+    // MARK: - Private methods
+    private func configureView() {
+        title = "Top Rated Movies"
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TopRatedMoviesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = String(indexPath.row)
+        
+        return cell
     }
 }
