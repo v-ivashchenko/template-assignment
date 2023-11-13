33a6b3a2b7dc58a65823c080fa3aafaadad56a0b
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2023-11-13 22:23:14 +0200

    Add extension to `XCTestCase` - `trackForMemoryLeaks`

---
+++ JatAppAssignmentTests/Shared/XCTestCase+trackForMemoryLeaks.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2023 Vitalii Ivashchenko. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
+        }
+    }
+}
