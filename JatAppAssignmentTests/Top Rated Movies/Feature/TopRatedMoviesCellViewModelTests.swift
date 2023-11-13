186c693ee753af986bde3d02f9f19f16b42b66b7
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2023-11-13 22:52:16 +0200

    Add tests for the `TopRatedMoviesCellViewModel`

---
+++ JatAppAssignmentTests/Top Rated Movies/Feature/TopRatedMoviesCellViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2023 Vitalii Ivashchenko. All rights reserved.
+//
+
+import XCTest
+@testable import JatAppAssignment
+
+final class TopRatedMoviesCellViewModelTests: XCTestCase {
+
+    func test_characterOccurrences_countsEveryLetter() {
+        let sut = TopRatedMoviesCellViewModel(title: "any title", imagePath: "any", rating: 0)
+        
+        XCTAssertEqual(sut.characterOccurrences, "a - 1, e - 1, i - 1, l - 1, n - 1, t - 2, y - 1")
+    }
+}
