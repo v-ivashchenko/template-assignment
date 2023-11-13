b4e93241cf0c8d9cb872bbccdc9b7c0116b91e3f
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2023-11-13 14:06:09 +0200

    Introduce `TopRatedMoviesDetailView`

---
+++ JatAppAssignment/Top Rated Movies Detail/Feature/TopRatedMoviesDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2023 Vitalii Ivashchenko. All rights reserved.
+//
+
+import UIKit
+
+struct TopRatedMoviesDetailViewModel {
+    
+    let title: String
+    let image: UIImage?
+    let rating: Double
+}
---
+++ JatAppAssignment/Top Rated Movies Detail/Presentation/TopRatedMoviesDetailView.swift
@@ -0,0 +1,84 @@
+//
+//  Copyright © 2023 Vitalii Ivashchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopRatedMoviesDetailView: View {
+    
+    let viewModel: TopRatedMoviesDetailViewModel
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                realView(image: image)
+            } else {
+                placeholderView
+            }
+        }
+        .foregroundStyle(foregroundColor)
+        .padding(Design.defaultPadding)
+    }
+    
+    // MARK: - Private properties
+    private var cornerRadius: CGFloat { 24 }
+    private var foregroundColor: some ShapeStyle { .gray.opacity(0.25) }
+    
+    private func realView(image: UIImage) -> some View {
+        VStack {
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxHeight: 300)
+            
+            Text(viewModel.title)
+            
+            UserScoreRatingViewRepresentable(value: viewModel.rating)
+                .frame(width: 48, height: 48)
+            
+            Spacer()
+        }
+    }
+    
+    private var placeholderView: some View {
+        VStack(alignment: .leading) {
+            ZStack {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.white)
+                    .frame(width: 128)
+            }
+            
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .frame(height: 25)
+            
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .frame(width: 300, height: 25)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    Group {
+        TopRatedMoviesDetailView(
+            viewModel: .init(
+                title: "A title",
+                image: UIImage(systemName: "photo"),
+                rating: 9.8
+            )
+        )
+        
+        TopRatedMoviesDetailView(
+            viewModel: .init(
+                title: "A title",
+                image: UIImage(systemName: "photo2"),
+                rating: 9.8
+            )
+        )
+    }
+}
